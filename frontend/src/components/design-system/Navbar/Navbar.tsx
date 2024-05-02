604f64bc5aa5be33144b953d143da8f784696714
Irina Garmaeva <garmaeva-i@mail.ru>
2024-05-02 21:08:26 +0200

    Feat: add Bracelets page

--- frontend/src/components/design-system/Navbar/Navbar.tsx
+++ frontend/src/components/design-system/Navbar/Navbar.tsx
@@ -7,38 +7,56 @@ type NavbarProps = {
   handleCloseMobileMenu: () => void;
 };
 
-const Navbar: FC<NavbarProps> = ({ isMobileMenuOpen, handleCloseMobileMenu }) => {
-  const {pathname} = useLocation()
+const Navbar: FC<NavbarProps> = ({
+  isMobileMenuOpen,
+  handleCloseMobileMenu,
+}) => {
+  const { pathname } = useLocation();
 
   return (
     <>
       {isMobileMenuOpen && (
         <nav className="fixed w-screen h-screen top-[90px] left-0 z-10 bg-white lg:hidden">
           <div className="flex flex-col">
-            <Link to="/" className={`hover-menu py-3 font-bold border-b border-[#d9d4d4] max-lg:pl-16 max-md:pl-5 ${pathname === '/' && 'bg-pink text-white hover:text-black'}`} onClick={handleCloseMobileMenu}>Home</Link>
+            <Link
+              to="/"
+              className={`hover-menu py-3 font-bold border-b border-[#d9d4d4] max-lg:pl-16 max-md:pl-5 ${
+                pathname === "/" && "bg-pink text-white hover:text-black"
+              }`}
+              onClick={handleCloseMobileMenu}
+            >
+              Home
+            </Link>
             {navItems.map((item) => (
-                <Link
-                  to={item.to}
-                  className="hover-menu py-3 font-bold border-b border-[#d9d4d4] max-lg:pl-16 max-md:pl-5"
-                  key={item.label}
-                  onClick={handleCloseMobileMenu}
-                >
-                  {item.label}
-                </Link>
+              <Link
+                to={item.to}
+                //className="hover-menu py-3 font-bold border-b border-[#d9d4d4] max-lg:pl-16 max-md:pl-5"
+                className={`hover-menu py-3 font-bold border-b border-[#d9d4d4] max-lg:pl-16 max-md:pl-5 ${
+                  pathname === item.to ? "text-pink" : ""
+                }`}
+                key={item.label}
+                onClick={handleCloseMobileMenu}
+              >
+                {item.label}
+              </Link>
             ))}
           </div>
         </nav>
       )}
-        <nav
-          id="menu"
-          className="flex flex-row justify-evenly items-center uppercase text-text-main font-semibold  max-lg:hidden"
-        >
-          {navItems.map((item) => (
-            <Link to={item.to} className="hover-menu" key={item.label}>
-              {item.label}
-            </Link>
-          ))}
-        </nav>
+      <nav
+        id="menu"
+        className="flex flex-row justify-evenly items-center uppercase text-text-main font-semibold  max-lg:hidden"
+      >
+        {navItems.map((item) => (
+          <Link
+            to={item.to}
+            className={`hover-menu ${pathname === item.to ? "text-pink underline underline-offset-4" : ""}`}
+            key={item.label}
+          >
+            {item.label}
+          </Link>
+        ))}
+      </nav>
     </>
   );
 };
